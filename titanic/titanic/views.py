6e37b0cc1dd7bbe566dc0f6561e66609443ccfa3
pybudy <manan.chauhan111@gmail.com>
2020-04-14 12:35:35 +0530

    first commit

---
+++ titanic/titanic/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from. import titanic_model
+def home(request):
+    return render(request, 'index.html')
+
+def prediction(request):
+    pclass = request.GET['pclass']
+    sex = request.GET['sex']
+    age = request.GET['age']
+    sibhp = request.GET['sibhp']
+    parch = request.GET['parch']
+    fare = request.GET['fare']
+    mbarked = request.GET['mbarked']
+    title = request.GET['title']
+
+
+    prediction = titanic_model.rfc(pclass,sex,age,sibhp,parch,fare,mbarked,title)
+
+    return render(request, 'prediction.html', {'prediction': prediction})
